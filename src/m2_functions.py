9f86d02bf3c530fbf47a04acbcfc47524f14cd16
lic8 <lic8@rose-hulman.edu>
2018-11-28 17:15:43 -0500

    DONE

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -4,7 +4,7 @@ Practice DEFINING and CALLING
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Aaron Wilkin, their colleagues, and Chen Li.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # DONE: 2.
@@ -56,7 +56,7 @@ def hypotenuse(a,b):
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -78,7 +78,7 @@ def hypotenuse(a,b):
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
